bd84e39bc012cae20a459bc78fea8750ebe2270c
baadhira <badirakunhabdulla@gmail.com>
2022-07-04 15:37:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let express = require("express");
+let cors = require("cors");
+let http = require("http");
+let bodyParser = require("body-parser");
+let path = require("path");
+
+const port=9000
+
+app = express();
+
+const server= http.createServer(app).listen(port, () => {});
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "client")));
+
+app.use(bodyParser.json());
+
+
+app.post("/server", (req, res) => {
+    io.emit("command", req.body);
+    console.log(req.body);
+
+    res.status(201).json({ status: "reached" });
+  });
+
+
+let io = require("socket.io")(server,{
+  cors: {
+    origin: "http://localhost:3000",
+}
+  });
+
+io.on("connection", (socket) => {
+    socket.on("command", function (data) {
+      io.emit("command", data);
+      console.log(data);
+    });
+  });
